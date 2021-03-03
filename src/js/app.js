1af1b04b4831e19b9010b6d7f9b18bab38eb58f7
Acauã <contato@acauamontiel.com.br>
2021-03-03 19:16:10 -0300

    add google tag manager

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,18 @@
 import '@babel/polyfill';
 import Theme from '@services/theme';
 import Vuelidate from 'vuelidate';
-import VueAnalytics from 'vue-analytics';
 import Money from 'v-money';
+import VueGtm from 'vue-gtm';
 import App from '@app/index.js';
 import main from '@components/main';
 import router from '@router';
 
 Vue.use(Vuelidate);
 Vue.use(Money, {precision: 2});
-Vue.use(VueAnalytics, {
-	id: 'G-WP4JNF02HQ',
-	router
+Vue.use(VueGtm, {
+	id: 'GTM-WK8WT72',
+	loadScript: true,
+	vueRouter: router
 });
 
 const app = new App(),
